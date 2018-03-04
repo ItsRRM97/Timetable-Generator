237cdf0b4e46a44c6cb65cf5ed6cbb5d8360e442
Harkirat <kirat2997@gmail.com>
2018-03-04 17:08:57 +0530

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ let PORT = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
 })
 
 app.listen(PORT, () =>{
